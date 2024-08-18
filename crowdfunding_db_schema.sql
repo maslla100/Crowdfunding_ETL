1901d5441847412bc306b7d945960890a626950b
luis llamas <luis.llamas@maslla.com>
2024-08-18 16:22:01 -0500

    added DB, Schema and pictures

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,9 @@
+
+copy category FROM '/Users/luisllamas/bootcamp/Data_Analytics/Homework/Project2/Crowdfunding_ETL/Reources/category.csv' DELIMITER ',' CSV HEADER;
+
+
+
+\copy subcategory FROM '/Users/luisllamas/bootcamp/Data_Analytics/Homework/Project2/Crowdfunding_ETL/Reources/subcategory.csv' DELIMITER ',' CSV HEADER;
+\copy contacts FROM '/Users/luisllamas/bootcamp/Data_Analytics/Homework/Project2/Crowdfunding_ETL/Reources/contacts.csv' DELIMITER ',' CSV HEADER;
+\copy campaign FROM '/Users/luisllamas/bootcamp/Data_Analytics/Homework/Project2/Crowdfunding_ETL/Reources/campaign.csv' DELIMITER ',' CSV HEADER;
+
